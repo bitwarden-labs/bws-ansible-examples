341e97e93f7f0055456b6f184a2d634ad844e705
Adam Thomas Jagger Bramley <atjbramley@hotmail.com>
2024-08-07 17:34:31 +0200

    remove ansible execution

--- certbox-nginx-vikunja/terraform/main.tf
+++ certbox-nginx-vikunja/terraform/main.tf
@@ -6,21 +6,22 @@ resource "digitalocean_droplet" "exampleserver" {
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
 
 
-  provisioner "remote-exec" {
-    connection {
-      host        = self.ipv4_address
-      user        = "root"
-      type        = "ssh"
-      private_key = file(var.digitalocean_ssh_key)
-      timeout     = "2m"
-    }
+  # provisioner "remote-exec" {
+  #   connection {
+  #     host        = self.ipv4_address
+  #     user        = "root"
+  #     type        = "ssh"
+  #     private_key = file(var.digitalocean_ssh_key)
+  #     timeout     = "2m"
+  #   }
 
-    inline = [
-      "apt-get update",
-      "apt-get install -y ansible",
-      "ansible-pull -U https://github.com/bitwarden-labs/bws-ansible-examples.git certbox-nginx-vikunja/runner.yml"
-    ]
-  }
+  #   inline = [
+  #     "apt-get update",
+  #     "apt-get install -y ansible",
+  #     # currently failing - unable to obtain lock file
+  #     "ansible-pull -U https://github.com/bitwarden-labs/bws-ansible-examples.git certbox-nginx-vikunja/runner.yml"
+  #   ]
+  # }
 }
 
 resource "digitalocean_project_resources" "bitwarden_tf" {
