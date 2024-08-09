523dbdd7206fd9136fb007f282ee6d48fe2ef113
Adam Thomas Jagger Bramley <atjbramley@hotmail.com>
2024-08-09 16:46:19 +0200

    support domain, subdomain and email vars

--- certbox-nginx-vikunja/certbot/certbot/get-certs.sh
+++ certbox-nginx-vikunja/certbot/certbot/get-certs.sh
@@ -5,8 +5,6 @@ set -x
 certbot certonly \
     --quiet \
     --agree-tos \
-    --email atjbramley@hotmail.com \
     --dns-cloudflare \
     --dns-cloudflare-credentials /root/certbot/cloudflare-credentials.ini \
     --dns-cloudflare-propagation-seconds 60 \
-    --domain 'vikunja.atjb.link'
